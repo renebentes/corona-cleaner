49b22086c23b0004f84b74ab842b7e32779281d5
Rene Bentes Pinto <renebentes@yahoo.com.br>
2020-04-01 16:32:40 -0300

    :construction: Adiciona funcão para alterar texto

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,1 +1,23 @@
-alert('corona-cleaner');
+const corona = /coronavírus|coronavirus|corona virus|corona vírus|covid-19|covid19|covid|corona/gi;
+const ignoreTags = ["script", "noscript", "style", "meta", "title"];
+
+function cure(element) {
+    if (ignoreTags.includes(element.localName)) return;
+
+    const text  = element.textContent;
+
+    element.textContent = text.replace(corona, "💉");
+}
+
+function find(element){
+    if (!element.childElementCount) {
+        cure(element);
+        return;
+    }
+
+    for (const el of element.children) {
+        find(el);
+    }
+}
+
+window.load = find(document.body);
