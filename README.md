a8bee1ba1578b91370bcf8b44ef6b36210dcd77b
Rene Bentes Pinto <renebentes@yahoo.com.br>
2020-04-03 15:10:25 -0300

    :art: Padroniza codificação

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ Remover as menções ao Coronavírus do conteúdo das páginas na Internet.
 
 ## Features
 
-* Localizar as menções ao Coronavírus
-* Aplicar a vacina
+- Localizar as menções ao Coronavírus
+- Aplicar a vacina
 
 ## Como usar?
 
@@ -38,5 +38,7 @@ chrome://extensions/
 6. Agora navegue curado do Coronavírus :)
 
 ---
+
 ## Ícone
+
 [Coronavirus icon](https://icons8.com.br/icons/set/coronavirus) por [Icons8](https://icons8.com.br)
--- src/scripts/content.js
+++ src/scripts/content.js
@@ -2,22 +2,22 @@ const corona = /(corona|covid)((-|\s)?(19|v[ií]rus))?/gi;
 const ignoreTags = ["script", "noscript", "style", "meta", "title"];
 
 function cure(element) {
-    if (ignoreTags.includes(element.localName)) return;
+  if (ignoreTags.includes(element.localName)) return;
 
-    const text  = element.textContent;
+  const text = element.textContent;
 
-    element.textContent = text.replace(corona, "💉");
+  element.textContent = text.replace(corona, "💉");
 }
 
-function process(element){
-    if (!element.childElementCount) {
-        cure(element);
-        return;
-    }
+function process(element) {
+  if (!element.childElementCount) {
+    cure(element);
+    return;
+  }
 
-    for (const el of element.children) {
-        process(el);
-    }
+  for (const el of element.children) {
+    process(el);
+  }
 }
 
 process(document.body);
